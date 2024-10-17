cf469dca8176b8a5dcb67071e5bd1a3ebc8e81f6
Joseph Nagel <JosephBNagel@gmail.com>
2024-10-17 09:43:17 +0200

    Add string permutation

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Some tech companies ask applicants to complete such exercises as part of the hir
 
 - [Fibonacci sequence](challenges/fibonacci.py)
 
+- [String permutation check](challenges/string_permutation.py)
+
 - [Non-maximum suppression](challenges/nms.py)
 
 - [Print directory tree](challenges/tree.py)
@@ -15,6 +17,10 @@ Some tech companies ask applicants to complete such exercises as part of the hir
 
 ```
 python challenges/fibonacci.py
+```
+
+```
+python challenges/string_permutation.py
 ```
 
 ```
---
+++ challenges/string_permutation.py
@@ -0,0 +1,43 @@
+'''String permutation check.'''
+
+def count_chars(a):
+    '''Count characters in string.'''
+
+    counts = {}
+
+    for char in a:
+        if char not in counts.keys():
+            counts[char] = 1
+        else:
+            counts[char] += 1
+
+    return counts
+
+
+def check_permutation(a, b, verbose=False):
+    '''Check whether two strings are permutations.'''
+
+    if len(a) != len(b):
+        is_permutation = False
+    else:
+        is_permutation = count_chars(a) == count_chars(b)
+
+    if verbose:
+        print(
+            '"{}" and "{}" are {}permutations'.format(
+                a, b, '' if is_permutation else 'not '
+            )
+        )
+
+    return is_permutation
+
+
+if __name__ == '__main__':
+
+    a = '123'
+    b = 'abc'
+    c = 'cba'
+
+    _ = check_permutation(a, b, verbose=True)
+    _ = check_permutation(b, c, verbose=True)
+
